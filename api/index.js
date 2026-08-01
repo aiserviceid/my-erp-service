608226018d739c1faa1039a2e336342d128030d2
Developer <developer@erpservice.local>
2026-08-01 22:37:39 +0700

    Fix Vercel serverless function wrapper

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
-const app = require('../server/index.cjs');
-
-module.exports = app;
+module.exports = (req, res) => {
+  try {
+    const app = require('../server/index.cjs');
+    return app(req, res);
+  } catch (err) {
+    console.error('Serverless Error:', err);
+    res.status(500).json({ error: 'Serverless Function Error', details: err.message, stack: err.stack });
+  }
+};
