a8e33c20021ec80c7f19d75fa097ee5a54a5b42a
aiserviceid <ai.interiorid@gmail.com>
2026-08-15 10:08:50 +0700

    ui: load shared dialog density fixes

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import './index.css'
 import './unitpro-ui-polish.css'
 import './unitpro-ui-stability.css'
 import './service-registration-compact.css'
+import './unitpro-dialog-density.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
